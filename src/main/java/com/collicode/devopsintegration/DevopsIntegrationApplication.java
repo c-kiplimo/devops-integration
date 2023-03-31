7ef704aca59b152153768e9f754c030f1ac0eec6
c-kiplimo <kiplimocollins855@gmail.com>
2023-03-31 05:59:39 +0300

    feat:welcome

--- src/main/java/com/collicode/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/collicode/devopsintegration/DevopsIntegrationApplication.java
@@ -2,10 +2,15 @@ package com.collicode.devopsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DevopsIntegrationApplication {
 
+	@GetMapping
+	public String message(){
+		return "Welcome to collicode";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
 	}
